a730a6bc48e29d284460478edc607f9090831c11
Henry (Wei) <hyd628@gmail.com>
2022-04-12 15:19:20 +0200

    base example

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -26,7 +26,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "moonbaseAlpha";
 
 const mainnetGwei = 21;
 
@@ -35,9 +35,9 @@ function mnemonic() {
     return fs.readFileSync("./mnemonic.txt").toString().trim();
   } catch (e) {
     if (defaultNetwork !== "localhost") {
-      console.log(
-        "☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`."
-      );
+      //console.log(
+      //  "☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`."
+      //);
     }
   }
   return "";
@@ -291,7 +291,7 @@ module.exports = {
   etherscan: {
     apiKey: {
       mainnet: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
-      // add other network's API key here
+      moonbaseAlpha: "U653VTJMIGF4938PRVUWBEC96ZPT4AU4FS"// add other network's API key here
     },
   },
 };
